9ed066dc15e39f002305ba9a844586b1d3bd9741
Nimit <nimitagg95@gmail.com>
2019-01-01 19:58:05 +0530

    moving error msg to constants
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- src/constants.js
+++ src/constants.js
@@ -19,3 +19,12 @@ module.exports.SOCKS_ADDRESS = {
   HOSTNAME: 0x03,
   IPV6: 0x04
 }
+
+module.exports.SOCKET_ERRORS = {
+  NOT_DEFINED : "Socket is not defined or valid",
+  WRONG_AUTH : "Wrong Authentication Details givem",
+  INITIAL_HANSHAKE : "Got Wrong initial Handshake(not 5,0,x)",
+  INVALID_AUTH_PACKET: "Invalid Authentication Packet received",
+  INVALID_CONNECT: "Got Wrong Handshake(not 5,1,x,x,x...)",
+  NO_CONNECTION : "Connection not established yet"
+}
--- src/index.js
+++ src/index.js
@@ -41,11 +41,11 @@ util.inherits(socks5Stream, Transform);
 socks5Stream.prototype._transform = function(chunk, encoding, callback) {
 
   if(!utils.isSocketObjValid(this._socket)) {
-    return callback(new Error("Socket is not defined or valid"));
+    return callback(new Error(constants.SOCKET_ERRORS.NOT_DEFINED));
   }
 
   if(this.authDetails && !utils.validateAuth(this.authDetails)) {
-    return callback(new Error("Wrong Authentication Details"));
+    return callback(new Error(constants.SOCKET_ERRORS.WRONG_AUTH));
   }
 
   // console.log(chunk);
@@ -53,7 +53,7 @@ socks5Stream.prototype._transform = function(chunk, encoding, callback) {
   if(this.state === constants.states.NO_CONNECTION) {
   
     if(!socksUtils.checkIntialSocksChunk(chunk)) {
-      return callback(new Error("Got Wrong initial Handshake(not 5,0,x)"));
+      return callback(new Error(constants.SOCKET_ERRORS.INITIAL_HANSHAKE));
     }
     //We need to conect 
     this._socket.write(socksUtils.generateInitialHandshakeResponse(this.authType), () => {
@@ -66,7 +66,7 @@ socks5Stream.prototype._transform = function(chunk, encoding, callback) {
 
   if(this.state === constants.states.AUTHENTICATION) {
     if(!socksUtils.checkAuthRequest(chunk, this.authType)) {
-      return callback(new Error("Wrong Authentication Details"));
+      return callback(new Error(constants.SOCKET_ERRORS.INVALID_AUTH_PACKET));
     }
 
     var authDetailRequest = socksUtils.getUsernamePassFromRequest(chunk, this.authType);
@@ -89,12 +89,12 @@ socks5Stream.prototype._transform = function(chunk, encoding, callback) {
 
   if(this.state === constants.states.CONNECTING) {
     if(!socksUtils.checkConnectResponse(chunk)) {
-      return callback(new Error("Got Wrong Handshake(not 5,1,x,x,x...)"));
+      return callback(new Error(constants.SOCKET_ERRORS.INVALID_CONNECT));
     }
 
     let hostnamePortObj = socksUtils.getHostnamePort(chunk);
 
-    return callback(null,`${hostnamePortObj.hostname},${hostnamePortObj.port}`);
+    return callback(null, `${hostnamePortObj.hostname}, ${hostnamePortObj.port}`);
   }
 
   if(this.state === constants.states.CONNECTED) {
@@ -107,7 +107,7 @@ socks5Stream.prototype._transform = function(chunk, encoding, callback) {
   }
   this.acceptConnection = function(flag) {
     if(this.state === constants.states.NO_CONNECTION) {
-      return callback(new Error("Connection not established yet."));
+      return callback(new Error(constants.SOCKET_ERRORS.NO_CONNECTION));
     }
 
     if(flag) {
