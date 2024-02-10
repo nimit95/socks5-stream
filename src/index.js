e9b42d382f85d8512e1d6963b3f9512721a9261a
ITMammad <ITMammad.Coder@gmail.com>
2024-02-10 16:31:57 +0330

    Fix Ports Identification. From little-endian to big-endian.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 
-const { Transform } = require("stream");
-const util = require("util");
-const constants = require("./constants"); 
-const socksUtils = require("./socksUtils");
-const utils = require("./utils");
+const { Transform } = require('stream');
+const util = require('util');
+const constants = require('./constants');
+const socksUtils = require('./socksUtils');
+const utils = require('./utils')
 
 /**
- * @param {net.Socket} socket 
+ * @param {net.Socket} socket
  *  Socket from which the Socks stream is comming.
  * @param {Object} authDetails
  *  Optional for authorization if any, Defaults to undefined
  * {
- *     //In case of username and password 
+ *     //In case of username and password
  *    authType:"username",
  *    username:<username>,
  *    password:<password>
- *    
- *  
+ *
+ *
  * }
  */
-const socks5Stream = function(socket, authDetails) {
+const socks5Stream = function (socket, authDetails) {
 
   this.authDetails = authDetails || false;
   this._socket = socket;
@@ -28,87 +28,89 @@ const socks5Stream = function(socket, authDetails) {
 
   this.state = constants.states.NO_CONNECTION;
 
-  if(this.authDetails) {
-    this.authType = constants.authTypes[this.authDetails.authType];
+  if (this.authDetails) {
+    this.authType = constants.authTypes[this.authDetails.authType]
   } else {
     this.authType = false;
   }
-};
+
+}
 
 util.inherits(socks5Stream, Transform);
 
-socks5Stream.prototype._transform = function(chunk, encoding, callback) {
+socks5Stream.prototype._transform = function (chunk, encoding, callback) {
 
-  if(!utils.isSocketObjValid(this._socket)) {
-    return callback(new Error(constants.SOCKET_ERRORS.NOT_DEFINED));
+  if (!utils.isSocketObjValid(this._socket)) {
+    return callback(new Error("Socket is not defined or valid"));
   }
 
-  if(this.authDetails && !utils.validateAuth(this.authDetails)) {
-    return callback(new Error(constants.SOCKET_ERRORS.WRONG_AUTH));
+  if (this.authDetails && !utils.validateAuth(this.authDetails)) {
+    return callback(new Error("Wrong Authentication Details"));
   }
 
   // console.log(chunk);
 
-  if(this.state === constants.states.NO_CONNECTION) {
-  
-    if(!socksUtils.checkIntialSocksChunk(chunk)) {
-      return callback(new Error(constants.SOCKET_ERRORS.INITIAL_HANSHAKE));
+  if (this.state === constants.states.NO_CONNECTION) {
+
+    if (!socksUtils.checkIntialSocksChunk(chunk)) {
+      return callback(new Error("Got Wrong initial Handshake(not 5,0,x)"));
     }
-    //We need to conect 
+    //We need to conect
     this._socket.write(socksUtils.generateInitialHandshakeResponse(this.authType), () => {
-      
-      this.state = this.authDetails? constants.states.AUTHENTICATION : constants.states.CONNECTING;
+
+      this.state = this.authDetails ? constants.states.AUTHENTICATION : constants.states.CONNECTING;
       callback(null);
     });
+
   }
 
-  if(this.state === constants.states.AUTHENTICATION) {
-    if(!socksUtils.checkAuthRequest(chunk, this.authType)) {
-      return callback(new Error(constants.SOCKET_ERRORS.INVALID_AUTH_PACKET));
+  if (this.state === constants.states.AUTHENTICATION) {
+    if (!socksUtils.checkAuthRequest(chunk, this.authType)) {
+      return callback(new Error("Wrong Authentication Details"));
     }
 
     var authDetailRequest = socksUtils.getUsernamePassFromRequest(chunk, this.authType);
 
-    if(this.authDetails.username == authDetailRequest.username && this.authDetails.password == authDetailRequest.password) {
+    if (this.authDetails.username == authDetailRequest.username && this.authDetails.password == authDetailRequest.password) {
       this._socket.write(socksUtils.generateSuccessSocksAuthResponse(), () => {
         this.state = constants.states.CONNECTING;
-      });
+      })
       callback(null);
     } else {
 
       this._socket.write(socksUtils.generateFailSocksAuthResponse(), () => {
         this.state = constants.states.DISCONNECTED;
-      });
+      })
       //Closing the connection in case of wrong username/password
       this._socket.end();
     }
 
   }
 
-  if(this.state === constants.states.CONNECTING) {
-    if(!socksUtils.checkConnectResponse(chunk)) {
-      return callback(new Error(constants.SOCKET_ERRORS.INVALID_CONNECT));
+  if (this.state === constants.states.CONNECTING) {
+    if (!socksUtils.checkConnectResponse(chunk)) {
+      return callback(new Error("Got Wrong Handshake(not 5,1,x,x,x...)"));
     }
 
     let hostnamePortObj = socksUtils.getHostnamePort(chunk);
 
-    return callback(null, `${hostnamePortObj.hostname}, ${hostnamePortObj.port}`);
+    return callback(null, `${hostnamePortObj.hostname},${hostnamePortObj.port}`);
   }
 
-  if(this.state === constants.states.CONNECTED) {
+  if (this.state === constants.states.CONNECTED) {
     // console.log('connected', chunk.toString());
     callback(null, chunk);
   }
 
-  if(this.state === constants.states.DISCONNECTED) {
+  if (this.state === constants.states.DISCONNECTED) {
     callback(null);
   }
-  this.acceptConnection = function(flag) {
-    if(this.state === constants.states.NO_CONNECTION) {
-      return callback(new Error(constants.SOCKET_ERRORS.NO_CONNECTION));
+  this.acceptConnection = function (flag) {
+    if (this.state === constants.states.NO_CONNECTION) {
+      return callback(new Error("Connection not established yet."));
     }
 
-    if(flag) {
+    if (flag) {
       this._socket.write(socksUtils.connectionSuccessfulResponse(), () => {
         this.state = constants.states.CONNECTED;
       });
@@ -119,15 +121,15 @@ socks5Stream.prototype._transform = function(chunk, encoding, callback) {
         //return callback(null);
       });
     }
-  };
+  }
 
-  this._socket.on("close", () => {
+  this._socket.on('close', () => {
     this.state = constants.states.DISCONNECTED;
   });
-  this._socket.on("error", () => {
+  this._socket.on('error', () => {
     this.state = constants.states.DISCONNECTED;
   });
-  
-};
+
+}
 
 module.exports = socks5Stream;
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,19 @@
 
-const constants = require("./constants"); 
+const constants = require('./constants');
 const utils = {
-  isSocketObjValid: function(socket) {
-    return socket && typeof socket == "object";
+  isSocketObjValid: function (socket) {
+    return socket && typeof socket == 'object'
   },
 
-  validateAuth: function(authDetails) {
-    if(typeof authDetails != "object"){
+  validateAuth: function (authDetails) {
+    if (typeof authDetails != 'object') {
       return false;
     }
-    if(constants.authTypes[authDetails.authType] === constants.authTypes["username"]) {
-      return typeof authDetails.username == "string" && typeof authDetails.password == "string"
-                    && authDetails.username.length > 0;
+    if (constants.authTypes[authDetails.authType] === constants.authTypes["username"]) {
+      return typeof authDetails.username == 'string' && typeof authDetails.password == 'string'
+        && authDetails.username.length > 0;
     }
   }
-};
+}
 
 module.exports = utils;
